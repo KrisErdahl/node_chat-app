72e8799da6cc6b4edc3ceef92e9cfa47093dec9b
KrisErdahl <>
2017-11-11 16:29:41 -0600

    Scroll to bottom if user is close to end

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 const socket = io();
 
+function scrollToBottom () {
+	// Selectors
+	const messages = jQuery('#messages');
+	const newMessage = messages.children('li:last-child');
+	// Heights
+	const clientHeight = messages.prop('clientHeight');
+	const scrollTop = messages.prop('scrollTop');
+	const scrollHeight = messages.prop('scrollHeight');
+	const newMessageHeight = newMessage.innerHeight();
+	const lastMessageHeight = newMessage.prev().innerHeight();
+
+	if (clientHeight+scrollTop+newMessageHeight+lastMessageHeight >= scrollHeight) {
+		// console.log('should scroll');
+		messages.scrollTop(scrollHeight);
+	}
+}
+
 socket.on('connect', function() {
 	console.log('connected to server');
 
@@ -24,6 +41,7 @@ socket.on('newMessage', function(message) {
 	});
 
 	jQuery('#messages').append(html);
+	scrollToBottom();
 	// console.log('new message', message);
 	// const formattedTime = moment(message.createdAt).format('h:mm a');
 	// const li = jQuery('<li></li>');
@@ -43,6 +61,7 @@ socket.on('newLocationMessage', function(message) {
 	});
 
 	jQuery('#messages').append(html);
+	scrollToBottom();
 	// const li = jQuery('<li></li>');
 	// const a = jQuery('<a target = "_blank">My Current Location</a>');
 	// li.text(`${message.from} at ${formattedTime}: `);
